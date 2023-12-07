8c056bf02f03f67b20b04d19b25b13ce31bc4cfb
sandradeutl <149825174+sandradeutl@users.noreply.github.com>
2023-12-06 22:30:10 -0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,15 +2,29 @@ extern read
 extern display
 extern weave
 extern printStats
-extern free
+extern freeMem
 section .data
 
-dv: dw 2
-msg: db "This is the original message.", 0
+msg1: db "This is the original message.", 0
+msg2: db "This is the original message.", 0
+msg3: db "This is the original message.", 0
+msg4: db "This is the original message.", 0
+msg5: db "This is the original message.", 0
+msg6: db "This is the original message.", 0
+msg7: db "This is the original message.", 0
+msg8: db "This is the original message.", 0
+msg9: db "This is the original message.", 0
+msg10: db "This is the original message.", 0
+
+msg_arr: dq msg1, msg2, msg3, msg4, msg5, msg6, msg7, msg8, msg9, msg10
+
 menuPrompt:     db "Encryption menu options:", 10 ,"s - show current messages", 10 ,"r - read new message", 10 ,"e - transform", 10 ,"p - print stats", 10 ,"q - quit program", 10 ,"enter option letter -> "
 menuPromptLen:  equ $- menuPrompt
 ;idk why the color is completely in string form though
 ; need to add the "Invalid option, try again!" part
+
+readPrompt      db "Please enter a string: ", 10 ;the string input is in assembly and the location input is in c because of convenience on both ends
+readPromptLen   equ $- readPrompt
 
 invalidPrompt:  db "Invalid option, try again!", 10
 invalidPromptLen:  equ $- invalidPrompt
@@ -21,8 +35,8 @@ catLen:         equ $- cat
 section .bss
 
 menuAns:        resb 2 ;one character + newLine
-
-zCounter:       resb 2 ;z counter
+zCounter:       resb 1 ;z counter
+newString       resb 100
 
 section .text
 
@@ -33,14 +47,13 @@ extern read
 extern display
 extern weave
 extern printStats
-extern free
+extern freeMem
 
 main:
     xor r8, r8
-    mov r10, zCounter
+    mov r10b, zCounter
     xor r10, r10 ;this will be the temporary z counter
 
-;allocate dynamic memory for string
 allocateStrMem:
     ; create dyn memory
     mov edi, 10 ;determine size later for now
@@ -118,7 +131,7 @@ comparing:
     jmp invalid ;else statemetn
 
 incrementCat:
-    mov r10, zCounter
+    mov r10b, zCounter
     inc r10
     cmp r10, 4 ;this only runs with the wrong letter
     je catPrint
@@ -126,66 +139,89 @@ incrementCat:
 
 
 optionDisplay:
+    mov r10b, zCounter
     xor r10, r10
     
-    mov rax, 1
-    mov rdi, 1
+    ;put parameters in place
+    mov rdi, msg_arr
     call display
-    syscall
 
     jmp prompt
 
 optionRead:
+    mov r10b, zCounter
     xor r10, r10
 
+    ;asking for string
     mov rax, 1
     mov rdi, 1
-    call read
+    mov rsi, readPrompt
+    mov rdx, readPromptLen
     syscall
+
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, newString
+    mov rdx, 100
+    syscall
+
+    ;rax also has the size of the string here??
+
+    ;put parameters in place
+    mov rdi, msg_arr
+    mov rsi, newString
+    call read
+
 
     jmp prompt
 
 ;need to randomly determine which to call
 ;need a je statement for whichever c function to call
 optionEncrypt:
+    mov r10b, zCounter
     xor r10, r10
-
-    mov rax, 1
-    mov rdi, 1
-    syscall
 
     jmp randChooseE
 
-randChooseE: ; test case this later
+randChooseE:
     rdrand eax ;generate and store a randomly generated number in eax
     mov ebx, 2
     div ebx
     
     cmp edx, 1 ; if the number is odd
     ja goReverse
-    jmp prompt
+    jmp goWeave
 
 goReverse:
     ;need to figure out how to pass a certain string to
+
+    ;put parameters in place
     
     jmp prompt
 
 goWeave:
+    ;put parameters in place
+    ;where to ask for location of the string array?
+    ;currently i am going to choose to ask for the location in  C because it's easier there
+    mov rdi, msg_arr
     call weave
 
     jmp prompt
 
 optionPrint:
+    mov r10b, zCounter
     xor r10, r10
 
-    mov rax, 1
-    mov rdi, 1
+    ;put parameters in place
+    mov rdi, msg_arr
     call printStats
-    syscall
 
     jmp prompt
 
 invalid:
+    mov r10b, zCounter
+    xor r10, r10
+
     mov rax, 1
     mov rdi, 1
     mov rsi, invalidPrompt
@@ -204,6 +240,9 @@ catPrint:
     syscall
 
 exit:
+    rdi msg_arr
+    call freeMem
+
     mov rax, 60 ;seg faults
     xor rdi, rdi
     syscall
