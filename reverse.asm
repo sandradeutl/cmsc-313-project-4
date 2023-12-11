93794472b46f640ad4c0ae84d4b9efca3b1ed599
klochart <87333285+klochart@users.noreply.github.com>
2023-12-10 21:05:39 -0500

    done

--- reverse.asm
+++ reverse.asm
@@ -1,5 +1,10 @@
-section .data
+; Sandra Deutl - 
+; Katheryne Lochart- MW24658
+; CMSC 313 Project 4 - reverse.asm from project 3
 
+extern scanf
+
+section .data
 numPrompt:      db "Enter a number between 2 and the total number of characters in the string: ", 0
 numPromptLen:   equ $- numPrompt
 
@@ -12,19 +17,51 @@ calcStrLen:     equ $- calcStr
 newLine:        db 10
 
 section .bss
+initialString:         resq 1
+editedStr:       resd 1
+num:            resd 1
+strLen:         resq 1
 
-string:         resb 100
-num:            resb 3
 
 section .text
 
-global main
+global reverse
+reverse:
 
-main:
     xor r8b, r8b
     xor r9, r9
     xor r12, r12
     xor r13, r13        ;just to make sure registers are null
+
+    push rbp
+    mov rbp, rsp
+    
+    xor r8, r8
+    mov r8, rdi
+    mov qword[initialString], r8
+
+    mov rsi, r8
+    mov rdi, editedStr
+
+    mov rcx, 0
+    mov rax, strLen
+    dec rax
+
+    call promptNum ; get number for splitting
+
+    call checkNum ; check if the number is between 2 and strLen
+    call printUnedited
+    call reverseFirstHalf
+
+    exor rdi, rdi
+    mov rcx, rsi
+    dec rcx
+
+    call reverseSecondHalf
+    call printEdited
+
+    ret
+
 
 promptNum:
     mov rax, 1
@@ -63,7 +100,7 @@ convertNumSingle:
     jl promptNum
     cmp r9b, r12b
     jg promptNum
-    dec r9b             ;this makes it print "correctly" - but it doesn't for the last one - fix that
+    dec r9b             
 
 
 printUnedited:
@@ -75,51 +112,65 @@ printUnedited:
 
     mov rax, 1
     mov rdi, 1
-    mov rsi, string
-    mov rdx, 64
+    mov rsi, [initialString]
+    xor rdx, rdx
+    mov edx, dword[strLen]
     syscall
+
+    call printNewLine
+
+    ret
+
+reverseFirstHalf:
+    cmp rdi, num
+    jae reverseSecondHalf
+
+    mov al, [resultStr + rdi]
+    mov [calcStr + rdi], al
+    inc rdi
+    jmp reverseFirstHalf
+
+
+reverseSecondHalf:
+    cmp rdi, num
+    je revLoopComplete
+
+    mov al, [resultStr + rdi]
+    mov [calcStr + rbx + rcx - rdi], al
+    inc rdi
+    jmp reverseSecondHalf
+
+
+reverseStrLoop:
+    cmp rcx, rax
+    jge revLoopComplete
+
+    mov al, [rsi + rcx]
+    mov [rdi], al
+    inc rdi
+    inc rcx
+    jmp reverseStrLoop
+
+revLoopComplete:
+    mov rax, 4
+    mov rbx, 1
+    mov rcx, calcStr
+    mov byte[rdi], 0
 
 printEdited:
     mov rax, 1
     mov rdi, 1
-    mov rsi, calcStr 
-    mov rdx, calcStrLen
+    mov rsi, [calcStr]
+    mov rdx, dword[calcStrLen]
     syscall
+
+    call printNewLine
 
     mov r8b, r9b
     mov r13, string
     add r13, r9         ;i wanted to do r9b but it won't let me
 
-printChar1:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, r13
-    mov rdx, 1
-    syscall
-
-moveThrough1:
-    dec r13
-    dec r8b
-    cmp r8b, 0          ;1 & not 0, bc it'll print char one more time after jumping
-    jge printChar1      ;if no jump, move to end
-    add r13, r12
-    add r8b, r12b       ;r12b bc it's a byte
-    cmp r8b, r9b        ;comparing first in case the entire string is reversed - because if the size of the string is input, entire string is reversed as one
-    je printNewLine
-
-
-printChar2:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, r13
-    mov rdx, 1          ;prints only one byte aka one character
-    syscall
-
-moveThrough2:
-    dec r13
-    dec r8b
-    cmp r8b, r9b
-    jg printChar2
+    ret
 
 printNewLine:
     mov rax, 1
